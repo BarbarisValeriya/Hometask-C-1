459cc3f0632a0492ca7c73d88c38cd9a650420e5
Valeriia <borisova.valeriya.n@mail.ru>
2023-10-22 12:22:19 +0300

    почти решено

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,13 +1,13 @@
 ﻿// Напишите программу, которая на вход принимает число N, а на выходе показывает все ЧЕТНЫЕ числа от 1 до N
 
 int N = 10;
-int start = 1;
+int start = 0;
 int count = start;
 
 while (count <= N)
     {
-    Console.Write(count % 2 == 0 + ", ");
-    count ++;
+    Console.Write(count + ", ");
+    count = count + 2;
     }
 
 
